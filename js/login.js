de9e448a6f26425b48e612a1fd02afa91b52a0f9
CesarGOD <cesar0in.16@gmail.com>
2023-05-27 13:54:02 -0600

    Se implemento el metodo para validar los usuarios

--- js/login.js
+++ js/login.js
@@ -1,25 +1,25 @@
 // Obtener una referencia al botón de ingresar mediante su ID
 const botonIngresar = document.getElementById("ingresar");
 
-
 // Agregar un evento de clic al botón de ingresar
 botonIngresar.addEventListener("click", function () {
-  // Obtener el valor del campo de nombre de usuario
+  // Obtener el valor del campo de nombre de usuario y contraseña
   const nombreUsuario = document.getElementById("userName").value;
+  const password = document.getElementById("password").value;
 
-  // Mostrar un mensaje emergente en el navegador con el nombre del usuario
-  alert("¡Bienvenido, " + nombreUsuario + "!");
+  // Buscar el usuario en el array de usuarios registrados
+  const usuarioEncontrado = usuariosRegistrados.find(function(usuario) {
+    return usuario.userName === nombreUsuario && usuario.password === password;
+  });
 
-  // Obtener la URL base actual
-  const urlActual = window.location.href;
-  const urlBase = urlActual.substring(0, urlActual.lastIndexOf("/") + 1);
-
-  // Concatenar la URL base con el nombre del archivo de la página deseada
-  const rutaLocal = "main.html";
-  const enlaceLocal = urlBase + rutaLocal;
-
-  // Redirigir a la página local
-  window.location.href = enlaceLocal;
+  // Verificar si se encontró el usuario
+  if (usuarioEncontrado) {
+    alert("¡Bienvenido, " + usuarioEncontrado.fullName + "!");
+    // Redirigir a la página principal después del inicio de sesión exitoso
+    window.location.href = "main.html";
+  } else {
+    alert("Nombre de usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+  }
 });
 
 
@@ -28,14 +28,16 @@ const botonRegistrar = document.getElementById("registrar");
 
 // Agregar un evento de clic al botón de registrar
 botonRegistrar.addEventListener("click", function() {
-// Obtener la URL base actual
-const urlActual = window.location.href;
-const urlBase = urlActual.substring(0, urlActual.lastIndexOf("/") + 1);
+  // Obtener la URL base actual
+  const urlActual = window.location.href;
+  const urlBase = urlActual.substring(0, urlActual.lastIndexOf("/") + 1);
 
-// Concatenar la URL base con la ruta de la página de registro
-const rutaRegistro = "RegistroU.html";
-const enlaceRegistro = urlBase + rutaRegistro;
+  // Concatenar la URL base con la ruta de la página de registro
+  const rutaRegistro = "RegistroU.html";
+  const enlaceRegistro = urlBase + rutaRegistro;
 
-// Redirigir a la página de registro
-window.location.href = enlaceRegistro;
+  // Redirigir a la página de registro
+  window.location.href = enlaceRegistro;
 });
+
+
